4282a055e12a21a612e8c2449801168752e608a3
Student <Student@nwu.ac.za>
2020-11-02 17:04:33 +0200

    ASP.Net Core MVC Web Application

---
+++ MvcCore 323 Project 2 Demo App/MvcCore 323 Project 2/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcCore_323_Project_2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        //
+        //Get : /Employee/Welcome/
+
+        public string Welcome()
+        {
+            return "This is the welcome action method...";
+        }
+    }
+}
---
+++ MvcCore 323 Project 2 Demo App/MvcCore 323 Project 2/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcCore_323_Project_2.Models;
+
+namespace MvcCore_323_Project_2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
